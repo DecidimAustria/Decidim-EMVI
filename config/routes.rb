0ec79cb3068ebbc4eb47eb7fc2f3596bcff2305b
Alexander Rusa <alex@rusa.at>
2025-06-25 19:46:48 +0200

    Add good_job dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  authenticate :user, ->(user) { user.admin? } do
+    mount GoodJob::Engine => 'good_job'
+  end
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
